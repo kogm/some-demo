cf95a5572ba4b71a5c20e512bd9e695bf3831cc4
kogm <15385654+kogm@users.noreply.github.com>
2020-03-25 01:49:28 +0800

    添加网络请求

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 window.onload = function() {
-  document.getElementById("main-content").innerHTML = "Hello, world";
+  document.getElementById("title").innerHTML = "Hello, world";
+  var button = document.getElementById("regist");
+
+  function req() {
+    var form_data = document.querySelectorAll("input");
+
+    var post_data = {
+      username: form_data[0].value,
+      password: form_data[1].value,
+      confirmPassword: form_data[1].value
+    };
+    // 发送请求
+    this.fetch("http://localhost:8088/passport/regist", {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json"
+      }),
+      body: JSON.stringify(post_data)
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.console.log("get data from server------>", data);
+      });
+  }
+
+  button.onclick = function() {
+    req();
+  };
 };
